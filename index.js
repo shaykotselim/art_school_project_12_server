d90a7b93085d3b3d816038a0815734b555d18e7b
shaykot43500 <hossainshaykot@gmail.com>
2023-06-14 00:29:32 +0600

    added class

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+//--------------------------------middleware----------------------------
+app.use(cors());
+app.use(express.json());
+
+// UserName: 
+// Password : 
+//---------------------Mongodb URI Client Here--------------------------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2bu1gse.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    
+    const classCollection = client.db("artschooldb").collection("showclass");
+
+    app.get('/showclass', async(req, res)=>{
+        const result = await classCollection.find().toArray();
+        res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+run().catch(console.dir);
+
+//--------------------------------------
+app.get('/', (req, res)=>{
+    res.json("Art School is Drawing");
+})
+
+app.listen(port, ()=>{
+    console.log(`Art school drawing on port ${port}`);
+})
