59661428626b0b5a75ca37d43d6a3a5dcb3311fe
shaykot43500 <hossainshaykot@gmail.com>
2023-06-16 04:45:33 +0600

    added new

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// const verifyJWT = (req, res, next)=>{
+//   const authorization = req.headers.authorization;
+//   if(!authorization){
+//     return res.status(401).send({error: true, message:'unauthorized access'});
+//   }
+//   // bearer token
+//   const token = authorization.split(' ')
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+//     if(err){
+//       return res.status(401).send({error: true, message: 'unauthorized access'})
+//     }
+//     req.decoded = decoded;
+//     next();
+//   })
+
+// }
 // UserName: 
 // Password : 
 //---------------------Mongodb URI Client Here--------------------------
@@ -34,8 +51,33 @@ async function run() {
     const instructorCollection = client.db("artschooldb").collection("showinstructor");
     const cartsCollection = client.db("artschooldb").collection("carts");
     const userCollection = client.db("artschooldb").collection("users");
-    
+    //jwt--------------
+
+    // app.post('/jwt', (req, res)=>{
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+
+    //   res.send({token})
+    // })
     //user related Api
+
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+     
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+    app.get("/users/instructor/:email", async (req, res) => {
+      const email = req.params.email;
+     
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
+
     app.get('/users', async (req, res)=>{
         const result = await userCollection.find().toArray();
         res.send(result);
@@ -59,6 +101,19 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users/instructor/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = { 
+        $set:{
+          role: 'instructor'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
     app.patch('/users/admin/:id', async(req, res)=>{
       const id = req.params.id;
       console.log(id);
@@ -79,6 +134,24 @@ async function run() {
         const result = await classCollection.find().toArray();
         res.send(result);
     })
+    app.post('/showclass', async(req, res)=>{
+      const clss = req.body;
+      // console.log(user);
+      // const query = {email: user.email};
+      // const existingUser = await userCollection.findOne(query);
+      // if(existingUser){
+      //     return res.send({message:'user already exists'})
+      // }
+
+      const result = await classCollection.insertOne(clss);
+      res.send(result);
+  });
+  app.get('/showclass/:email', async(req, res)=>{
+    const email = req.params.email
+    const query = {instructor_email:email} 
+    const result = await classCollection.find(query).toArray()
+    res.send(result)
+  })
     // Instructor Area Here--------------
     app.get('/showinstructor', async(req, res)=>{
         const result = await instructorCollection.find().toArray();
@@ -87,9 +160,17 @@ async function run() {
     // Cart Collection Post  Area here............
     app.get('/carts', async(req, res)=>{
         const email = req.query.email;
-        if(!email){
-            res.send([]);
-        }
+
+        // jwt Area-------------------------------Start.
+        // if(!email){
+        //     res.send([]);
+        // }
+        // const decodedEmail = req.decoded.email;
+        // if(email !== decodedEmail){
+        //     return res.status(401).send( {error: true, message:'probidden access'})
+        // }
+        // Jwt Area --------------------------------End.
+
         const query ={email: email};
         const result = await cartsCollection.find(query).toArray();
         res.send(result)
